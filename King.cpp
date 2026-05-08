fe853a21d5138df42f6c5105d266a5a7e298e906
Ahsan Tahir <muhammadahsantahir77@gmail.com>
2026-05-08 16:57:19 +0500

    King done

--- King.cpp
+++ King.cpp
@@ -1,1 +1,46 @@
-//Ahsan
+#include"King.h"
+#include"Board.h"
+#include<cmath>
+
+// Constructor for King
+
+King::King(char c, int row, int col) : Piece(c, row, col)
+{ }
+
+bool King::isValidMove(int toRow, int toCol, const Board& board) const
+{
+	// x is the starting row
+	//y is the starting column 
+
+	int rowDiff = abs(toRow - x);
+	int colDiff = abs(toCol - y);
+
+	// King can move one square in any direction, so both row and column differences must be 1
+	if (!board.isInsideBoard(toRow, toCol))
+	{
+		return false;
+	}
+	if (rowDiff > 1 && colDiff > 1)
+	{
+		return false;
+	}
+	// Pointer to the piece at the destination 
+	Piece* destination = board.getPiece(toRow, toCol);
+
+	if (destination == nullptr || destination->getColor() != color)
+	{
+		//Piece captured or moved to an empty square
+		return true;
+	}
+	return false; // Destination occupied by same color piece
+
+}
+char King::getSymbol()
+{
+	// Returning 'K' for white king and 'k' for black king
+
+	if (color == 'W')
+		return 'K';
+
+	return 'k';
+}
